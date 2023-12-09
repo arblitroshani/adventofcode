1103388960441e49a3f45b8f3661afa0b902756a
Arbli Troshani <arbli@staffbase.com>
2023-12-09 11:15:03 +0100

    optimize day9 pt2 by reusing pt1 with a reversed string

--- src/main/kotlin/com/arblitroshani/adventofcode/year2023/Day9.kt
+++ src/main/kotlin/com/arblitroshani/adventofcode/year2023/Day9.kt
@@ -3,22 +3,20 @@ package com.arblitroshani.adventofcode.year2023
 import com.arblitroshani.adventofcode.util.InputReader
 import com.arblitroshani.adventofcode.util.print
 
-fun main() =
-    InputReader(2023, 9).read()
+fun main() {
+    val numberLines = InputReader(2023, 9).read()
         .map { it.split(" ").map(String::toInt) }
-        .map { numbers ->
-            var values = numbers
-            var next = 0
-            val firstElements = mutableListOf<Int>()
-            while (!values.all { it == 0 }) {
-                next += values.last()
-                firstElements.add(values[0])
-                values = values.windowed(2).map { it[1] - it[0] }
-            }
-            next to firstElements.reversed()
-        }
-        .fold(0 to 0) { (sum1, sum2), (next, elements) ->
-            val prev = elements.zipWithNext().fold(elements[0]) { acc, (_, b) -> b - acc }
-            sum1 + next to sum2 + prev
-        }
-        .print()
+
+    numberLines.sumOf { resolve(it) }.print()
+    numberLines.sumOf { resolve(it.reversed()) }.print()
+}
+
+fun resolve(numbers: List<Int>): Int {
+    var values = numbers
+    var next = 0
+    while (!values.all { it == 0 }) {
+        next += values.last()
+        values = values.windowed(2).map { it[1] - it[0] }
+    }
+    return next
+}
