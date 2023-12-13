91ae29b2cf9918ff3eeb97bd894bd3273c947b36
Arbli Troshani <arbli@staffbase.com>
2023-12-13 13:59:10 +0100

    add day 13

--- src/main/kotlin/com/arblitroshani/adventofcode/util/extension/ListExtension.kt
+++ src/main/kotlin/com/arblitroshani/adventofcode/util/extension/ListExtension.kt
@@ -6,4 +6,9 @@ fun List<Int>.hasSingleZeroElement() = this == listOf(0)
 
 val List<Int>.memoKey: String get() = fold("") { acc, e -> "$acc.$e" }
 
+fun List<String>.columns(): List<String> =
+    (0 until first().length).map { j ->
+        map { it[j] }.joinToString(separator = "")
+    }
+
 fun String.toIntList(): List<Int> = split(',').map(String::toInt)
