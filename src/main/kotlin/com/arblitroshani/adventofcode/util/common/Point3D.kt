78efb56c436e48785df65fa3c145f4cd6a601b53
Arbli Troshani <arbli@staffbase.com>
2023-12-25 18:53:35 +0100

    add day 24 part 1

---
+++ src/main/kotlin/com/arblitroshani/adventofcode/util/common/Point3D.kt
@@ -0,0 +1,7 @@
+package com.arblitroshani.adventofcode.util.common
+
+data class Point3D(
+    val x: Long,
+    val y: Long,
+    val z: Long,
+)
--- src/main/kotlin/com/arblitroshani/adventofcode/util/extension/ListExtension.kt
+++ src/main/kotlin/com/arblitroshani/adventofcode/util/extension/ListExtension.kt
@@ -4,11 +4,14 @@ fun List<Int>.decrementFirst(): List<Int> = toMutableList().apply { this[0]-- }
 
 fun List<Int>.hasSingleZeroElement() = this == listOf(0)
 
-val List<Int>.memoKey: String get() = fold("") { acc, e -> "$acc.$e" }
-
 fun List<String>.columns(): List<String> =
     (0 until first().length).map { j ->
         map { it[j] }.joinToString(separator = "")
     }
 
 fun String.toIntList(): List<Int> = split(',').map(String::toInt)
+
+fun <T> List<T>.getAllPairs(): List<Pair<T, T>> =
+    flatMapIndexed { index, value ->
+        subList(index + 1, size).map { other -> Pair(value, other) }
+    }
