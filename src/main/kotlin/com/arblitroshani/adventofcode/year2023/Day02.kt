47f680ecd6d381c33a2552b43020b24ffad3356b
Arbli Troshani <arbli@staffbase.com>
2024-01-15 12:17:45 +0100

    use template in day 2

--- src/main/kotlin/com/arblitroshani/adventofcode/year2023/Day02.kt
+++ src/main/kotlin/com/arblitroshani/adventofcode/year2023/Day02.kt
@@ -1,53 +1,58 @@
 package com.arblitroshani.adventofcode.year2023
 
-import com.arblitroshani.adventofcode.util.InputReader
-import com.arblitroshani.adventofcode.util.println
+import com.arblitroshani.adventofcode.AocPuzzle
 
-fun main() {
-    val input = InputReader(2023, 2).read()
+typealias Input23d02 = List<String>
 
-    var sumPt1 = 0
-    var sumPt2 = 0
+class Day02: AocPuzzle<Input23d02>() {
 
-    input.forEachIndexed { index, line ->
-        val game = line.split(":").last().trim()
-        sumPt2 += powerOfGame(game)
-        if (isGamePossible(game)) sumPt1 += (index + 1)
+    override fun parseInput(lines: List<String>) =
+        lines.map { it.split(':').last().trim() }
+
+    override fun partOne(): Int =
+        input
+            .mapIndexed { index, game -> game to index}
+            .filter { isGamePossible(it.first) }
+            .sumOf { it.second + 1 }
+
+    override fun partTwo(): Int =
+        input.sumOf(::powerOfGame)
+
+    private fun isGamePossible(game: String): Boolean {
+        for (gamePart in game.split(";").map(String::trim))
+            for (pick in gamePart.split(",").map(String::trim)) {
+                val value = pick.split(" ").first().toInt()
+                if (pick.endsWith("red") && value > 12) return false
+                if (pick.endsWith("green") && value > 13) return false
+                if (pick.endsWith("blue") && value > 14) return false
+            }
+        return true
     }
 
-    sumPt1.println()
-    sumPt2.println()
+    private fun powerOfGame(game: String): Int {
+        var r = 0
+        var g = 0
+        var b = 0
+
+        game
+            .split(";")
+            .map(String::trim)
+            .forEach { gamePart ->
+                gamePart
+                    .split(",")
+                    .map(String::trim)
+                    .forEach { pick ->
+                        val value = pick.split(" ").first().toInt()
+                        if (pick.endsWith("red")) r = maxOf(r, value)
+                        if (pick.endsWith("green")) g = maxOf(g, value)
+                        if (pick.endsWith("blue")) b = maxOf(b, value)
+                    }
+            }
+        return r * g * b
+    }
 }
 
-fun isGamePossible(game: String): Boolean {
-    for (gamePart in game.split(";").map(String::trim))
-        for (pick in gamePart.split(",").map(String::trim)) {
-            val value = pick.split(" ").first().toInt()
-            if (pick.endsWith("red") && value > 12) return false
-            if (pick.endsWith("green") && value > 13) return false
-            if (pick.endsWith("blue") && value > 14) return false
-        }
-    return true
-}
-
-fun powerOfGame(game: String): Int {
-    var r = 0
-    var g = 0
-    var b = 0
-
-    game
-        .split(";")
-        .map(String::trim)
-        .forEach { gamePart ->
-            gamePart
-                .split(",")
-                .map(String::trim)
-                .forEach { pick ->
-                    val value = pick.split(" ").first().toInt()
-                    if (pick.endsWith("red")) r = maxOf(r, value)
-                    if (pick.endsWith("green")) g = maxOf(g, value)
-                    if (pick.endsWith("blue")) b = maxOf(b, value)
-                }
-        }
-    return r * g * b
-}
+fun main() = Day02().solve(
+    expectedAnswerForSampleInP1 = 8,
+    expectedAnswerForSampleInP2 = 2286,
+)
