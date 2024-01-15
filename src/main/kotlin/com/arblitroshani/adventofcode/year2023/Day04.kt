0039d47dd378fd8ff6d7c8ac18dd5055a8cbf954
Arbli Troshani <arbli@staffbase.com>
2024-01-15 12:35:34 +0100

    use template in day 4

--- src/main/kotlin/com/arblitroshani/adventofcode/year2023/Day04.kt
+++ src/main/kotlin/com/arblitroshani/adventofcode/year2023/Day04.kt
@@ -1,21 +1,34 @@
 package com.arblitroshani.adventofcode.year2023
 
-import com.arblitroshani.adventofcode.util.InputReader
-import com.arblitroshani.adventofcode.util.println
+import com.arblitroshani.adventofcode.AocPuzzle
 
-fun main() {
-    val input = InputReader(2023, 4).read()
-    val copies = Array(input.size) { 1 }
-    val sumPt1 = input.mapIndexed { index, line ->
-        line.split(":").last().trim()
-            .split("|").map { it.split(" ").filterNot(String::isBlank).toSet() }
-            .reduce(Set<String>::intersect).size
-            .let { ticketsWon ->
-                for (i in 1 .. ticketsWon) copies[index + i] += copies[index]
-                1 shl ticketsWon - 1
-            }
-    }.sum()
+typealias Input23d04 = List<String>
 
-    sumPt1.println()
-    copies.sum().println()
+class Day04: AocPuzzle<Input23d04>() {
+
+    private var copies: Array<Int> = emptyArray()
+
+    override fun parseInput(lines: List<String>): Input23d04 {
+        copies = Array(lines.size) { 1 }
+        return lines
+    }
+
+    override fun partOne(): Int =
+        input.mapIndexed { index, line ->
+            line.split(":").last().trim()
+                .split("|")
+                .map { it.split(" ").filterNot(String::isBlank).toSet() }
+                .reduce(Set<String>::intersect).size
+                .let { ticketsWon ->
+                    for (i in 1 .. ticketsWon) copies[index + i] += copies[index]
+                    1 shl ticketsWon - 1
+                }
+        }.sum()
+
+    override fun partTwo(): Int = copies.sum()
 }
+
+fun main() = Day04().solve(
+    expectedAnswerForSampleInP1 = 13,
+    expectedAnswerForSampleInP2 = 30,
+)
