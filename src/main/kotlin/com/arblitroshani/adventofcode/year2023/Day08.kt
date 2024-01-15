5022f4e58d8e8f65b8137dc7fd76059767d811a0
Arbli Troshani <arbli@staffbase.com>
2024-01-15 13:04:15 +0100

    use template in day 8

--- src/main/kotlin/com/arblitroshani/adventofcode/year2023/Day08.kt
+++ src/main/kotlin/com/arblitroshani/adventofcode/year2023/Day08.kt
@@ -1,46 +1,56 @@
 package com.arblitroshani.adventofcode.year2023
 
-import com.arblitroshani.adventofcode.util.InputReader
+import com.arblitroshani.adventofcode.AocPuzzle
 import com.arblitroshani.adventofcode.util.lcm
-import com.arblitroshani.adventofcode.util.println
 
-fun main() {
-    val ip = InputReader(2023, 8).read()
+data class Input23d08(
+    val instructions: List<Boolean>,
+    val elements: Map<String, String>,
+)
 
-    val instructions = ip[0].map { it == 'L' }
-    val elements = mutableMapOf<String, String>()
-    ip.drop(2).forEach { line ->
-        val (name, l, r) = line.split(" = (", ", ", ")")
-        elements["${name}l"] = l
-        elements["${name}r"] = r
+class Day08: AocPuzzle<Input23d08>() {
+
+    override fun parseInput(lines: List<String>): Input23d08 {
+        val elements = mutableMapOf<String, String>()
+        lines.drop(2).forEach { line ->
+            val (name, l, r) = line.split(" = (", ", ", ")")
+            elements["${name}l"] = l
+            elements["${name}r"] = r
+        }
+        return Input23d08(
+            instructions = lines[0].map { it == 'L' },
+            elements = elements,
+        )
     }
 
-    stepsForPart1(instructions, elements).println()
-    stepsForPart2(instructions, elements).println()
-}
+    override fun partOne(): Long =
+        stepsForPart1("AAA") { it.endsWith("ZZZ") }
 
-fun stepsForPart1(
-    instructions: List<Boolean>,
-    elements: Map<String, String>,
-    startElement: String = "AAA",
-    isComplete: (String) -> Boolean = { it == "ZZZ" },
-): Long {
-    var currentEl = startElement
-    var steps = 0L
-    while (!isComplete(currentEl)) {
-        val index = steps % (instructions.size).toLong()
-        val instruction = instructions[index.toInt()]
-        currentEl = elements[currentEl + if (instruction) 'l' else 'r'] ?: throw Exception()
-        steps += 1
+    override fun partTwo(): Long =
+        input.elements.keys
+            .asSequence()
+            .map { it.dropLast(1) }
+            .filter { it.endsWith('A') }
+            .distinct()
+            .map { el -> stepsForPart1(el) { it.endsWith('Z') } }
+            .fold(1, ::lcm)
+
+    private fun stepsForPart1(
+        currentElement: String,
+        isComplete: (String) -> Boolean,
+    ): Long {
+        var currentEl = currentElement
+        var steps = 0L
+        while (!isComplete(currentEl)) {
+            val index = steps % (input.instructions.size).toLong()
+            val instruction = input.instructions[index.toInt()]
+            currentEl = input.elements[currentEl + if (instruction) 'l' else 'r'] ?: throw Exception()
+            steps += 1
+        }
+        return steps
     }
-    return steps
 }
 
-fun stepsForPart2(instructions: List<Boolean>, elements: Map<String, String>): Long =
-    elements.keys
-        .asSequence()
-        .map { it.dropLast(1) }
-        .filter { it.endsWith('A') }
-        .distinct()
-        .map { el -> stepsForPart1(instructions, elements, el) { it.endsWith('Z') } }
-        .fold(1, ::lcm)
+fun main() = Day08().solve(
+    expectedAnswerForSampleInP1 = 2L,
+)
