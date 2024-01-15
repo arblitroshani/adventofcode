02fccaf81782e9d998ddba5fa4a55eab7dfce3b8
Arbli Troshani <arbli@staffbase.com>
2024-01-15 13:07:32 +0100

    use template in day 9

--- src/main/kotlin/com/arblitroshani/adventofcode/year2023/Day09.kt
+++ src/main/kotlin/com/arblitroshani/adventofcode/year2023/Day09.kt
@@ -1,22 +1,32 @@
 package com.arblitroshani.adventofcode.year2023
 
-import com.arblitroshani.adventofcode.util.InputReader
-import com.arblitroshani.adventofcode.util.println
+import com.arblitroshani.adventofcode.AocPuzzle
 
-fun main() {
-    val numberLines = InputReader(2023, 9).read()
-        .map { it.split(" ").map(String::toInt) }
+typealias Input23d09 = List<List<Int>>
 
-    numberLines.sumOf { resolve(it) }.println()
-    numberLines.sumOf { resolve(it.reversed()) }.println()
-}
+class Day09: AocPuzzle<Input23d09>() {
 
-fun resolve(numbers: List<Int>): Int {
-    var values = numbers
-    var next = 0
-    while (!values.all { it == 0 }) {
-        next += values.last()
-        values = values.windowed(2).map { it[1] - it[0] }
+    override fun parseInput(lines: List<String>) =
+        lines.map { it.split(" ").map(String::toInt) }
+
+    override fun partOne(): Int =
+        input.sumOf(::resolve)
+
+    override fun partTwo(): Int =
+        input.sumOf { resolve(it.reversed()) }
+
+    private fun resolve(numbers: List<Int>): Int {
+        var values = numbers
+        var next = 0
+        while (!values.all { it == 0 }) {
+            next += values.last()
+            values = values.windowed(2).map { it[1] - it[0] }
+        }
+        return next
     }
-    return next
 }
+
+fun main() = Day09().solve(
+    expectedAnswerForSampleInP1 = 114,
+    expectedAnswerForSampleInP2 = 2,
+)
