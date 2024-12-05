67fe0a58aa777983593f3b4b4f0e5f7ff49cba9b
Arbli Troshani <arbli@staffbase.com>
2024-12-05 07:52:40 +0100

    add 2024/day5 solution

---
+++ src/main/kotlin/year2024/day05/Solution.kt
@@ -0,0 +1,95 @@
+package year2024.day05
+
+import framework.solution
+import util.InputParsing
+
+private typealias OrderingRule = Pair<Int, Int>
+private typealias Update = MutableList<Int>
+private data class Input(val rules: List<OrderingRule>, val updates: List<Update>)
+
+fun main() = solution<Input>(2024, 5) {
+
+    fun Update.isCorrect(rules: List<OrderingRule>): Boolean =
+        windowed(2)
+            .map { (a, b) -> a to b }
+            .all(rules::contains)
+
+    fun Update.middle(): Int = this[size / 2]
+
+    parseInput { lines ->
+        val (orderingRulesLines, updatesLines) = InputParsing.splitListByEmptyLines(lines)
+        Input(
+            rules = orderingRulesLines.map { line ->
+                val (n1, n2) = line.split('|').map(String::toInt)
+                n1 to n2
+            },
+            updates = updatesLines.map { line ->
+                line.split(',').map(String::toInt).toMutableList()
+            },
+        )
+    }
+
+    partOne { input ->
+        input.updates
+            .filter { it.isCorrect(input.rules) }
+            .sumOf(Update::middle)
+    }
+
+    partTwo { input ->
+        fun Update.swapping(i: Int, j: Int): Update = apply {
+            remove(i)
+            add(indexOf(j), i)
+        }
+
+        fun Update.fixed(): Update {
+            val (i, j) = windowed(2)
+                .map { (a, b) -> b to a }
+                .firstOrNull(input.rules::contains) ?: return this
+            return swapping(i, j).fixed()
+        }
+
+        input.updates
+            .filterNot { it.isCorrect(input.rules) }
+            .map(Update::fixed)
+            .sumOf(Update::middle)
+    }
+
+    val testInput = """
+        47|53
+        97|13
+        97|61
+        97|47
+        75|29
+        61|13
+        75|53
+        29|13
+        97|29
+        53|29
+        61|53
+        97|53
+        61|29
+        47|13
+        75|47
+        97|75
+        47|61
+        75|61
+        47|29
+        75|13
+        53|13
+
+        75,47,61,53,29
+        97,61,53,29,13
+        75,29,13
+        75,97,47,61,53
+        61,13,29
+        97,13,75,29,47
+    """.trimIndent()
+
+    partOneTest {
+        testInput shouldOutput 143
+    }
+
+    partTwoTest {
+        testInput shouldOutput 123
+    }
+}
