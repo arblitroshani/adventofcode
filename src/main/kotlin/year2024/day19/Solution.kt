b271288c7914b6a2dea213f56d97d9954657b362
Arbli Troshani <arbli@staffbase.com>
2024-12-20 20:26:16 +0100

    improve 2024/day19 solution

--- src/main/kotlin/year2024/day19/Solution.kt
+++ src/main/kotlin/year2024/day19/Solution.kt
@@ -3,72 +3,56 @@ package year2024.day19
 import framework.solution
 import util.InputParsing
 
-private data class Input(val root: TrieNode, val towels: List<String>)
-
 private data class TrieNode(
     val nodes: MutableMap<Char, TrieNode> = mutableMapOf(),
     var isLeaf: Boolean = false,
 )
 
-fun main() = solution<Input>(2024, 19) {
+fun main() = solution<Pair<TrieNode, List<String>>>(2024, 19) {
 
-    parseInput { lines ->
-        val (patternLines, towelLines) = InputParsing.splitListByEmptyLines(lines)
-        val root = TrieNode()
-        patternLines.first()
-            .split(", ")
-            .filterNot(String::isEmpty)
-            .forEach { pattern ->
-                var currentTrieNode = root
-                pattern.forEachIndexed { i, ch ->
-                    val newNode = currentTrieNode.nodes.getOrPut(ch) { TrieNode() }
-                    if (i == pattern.length - 1) newNode.isLeaf = true
-                    currentTrieNode = newNode
-                }
-            }
-        Input(root, towelLines)
-    }
+    val memo = mutableMapOf<String, Long>()
 
     fun getIndices(towel: String, root: TrieNode): List<Int> {
         val indices = mutableListOf<Int>()
         var trieNode = root
         var i = 0
         while (i < towel.length) {
-            if (!trieNode.nodes.containsKey(towel[i])) break
-            val newNode = trieNode.nodes.getOrElse(towel[i]) { error("") }
-            if (newNode.isLeaf) indices.add(i + 1)
-            i++
-            trieNode = newNode
+            trieNode = trieNode.nodes
+                .getOrElse(towel[i++]) { return indices }
+                .also { if (it.isLeaf) indices.add(i) }
         }
         return indices
     }
 
-    partOne { (root, towels) ->
-        val memo = mutableMapOf<String, Boolean>()
+    fun countValid(towel: String, root: TrieNode): Long = when {
+        towel.isEmpty() -> 1
+        else -> memo.getOrPut(towel) {
+            getIndices(towel, root).sumOf { countValid(towel.drop(it), root) }
+        }
+    }
 
-        fun isValid(towel: String): Boolean =
-            towel.isEmpty() || memo.getOrPut(towel) {
-                return@getOrPut getIndices(towel, root).any {
-                    isValid(towel.drop(it))
+    parseInput { lines ->
+        val (patternLines, towelLines) = InputParsing.splitListByEmptyLines(lines)
+        val root = TrieNode()
+        patternLines.first()
+            .split(", ")
+            .forEach { pattern ->
+                pattern.foldIndexed(root) { i, acc, c ->
+                    acc.nodes.getOrPut(c) { TrieNode() }.also {
+                        if (i == pattern.length - 1) it.isLeaf = true
+                    }
                 }
             }
+        root to towelLines
+    }
 
-        towels.count(::isValid)
+    partOne { (root, towels) ->
+        towels.map { countValid(it, root) }.count { it > 0 }
     }
 
     partTwo { (root, towels) ->
-        val memo = mutableMapOf<String, Long>()
-
-        fun countValid(towel: String): Long = when {
-            towel.isEmpty() -> 1
-            else -> memo.getOrPut(towel) {
-                return@getOrPut getIndices(towel, root).sumOf {
-                    countValid(towel.drop(it))
-                }
-            }
-        }
-
-        towels.sumOf(::countValid)
+        memo.clear()
+        towels.sumOf { countValid(it, root) }
     }
 
     val testInput = """
